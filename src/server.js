2aecad328987002f5ce08076ece7c40cf745eb5f
cooperbraun13 <cooperjbraun13@gmail.com>
2025-08-11 01:10:03 -0700

    feat: finish server and set up basic db

--- src/server.js
+++ src/server.js
@@ -10,11 +10,12 @@ const __filename = fileURLToPath(import.meta.url);
 // Get the directory name from the file path
 const __dirname = dirname(__filename);
 
+// Middleware
+app.use(express.json());
 // Serves the HTML file from the /public directory
 // Tells express to serve all files from the public folder as static assets / file,
 // any requests for the css files will be resolved to the public directory
-
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Serving up the HTML file from the /public directory
 app.get('/', (req, res) => {
